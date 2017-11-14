a3a6160fb95c436679e8ee567ec0a7e43b91636a
gambani-simran <gambanisimran08@gmail.com>
2017-11-14 20:28:39 +0530

    basics

--- BasicDataTypes/Floor.py
+++
@@ -1,9 +0,0 @@
-# https://www.hackerrank.com/challenges/floor-ceil-and-rint/problem
-
-import numpy
-arr = raw_input().strip().split(' ')
-
-a = numpy.array(arr,dtype=float)
-print numpy.floor(a)
-print numpy.ceil(a)
-print numpy.rint(a)
--- BasicDataTypes/MinAndMax.py
+++
@@ -1,9 +0,0 @@
-# https://www.hackerrank.com/challenges/np-min-and-max/problem
-
-import numpy
-arr = raw_input().strip().split(' ')
-n = int(arr[0])
-m = int(arr[1])
-
-a1 = numpy.array([list(map(int,raw_input().split(' '))) for i in range(0,n)])
-print numpy.max(numpy.min(a1,axis=1))
