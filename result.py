5533641d8e4f620c3a25ea7c8cc67883ba0c5b00
gambani-simran <gambanisimran08@gmail.com>
2017-12-30 17:04:48 +0530

    ui done

--- result.py
+++ result.py
@@ -4,12 +4,13 @@ from sklearn import datasets, svm, tree, preprocessing, metrics
 import sklearn.ensemble as ske
 import numpy as np
 from sklearn.model_selection import train_test_split
+from flask import Flask, redirect, url_for, request
 import Recommenders
 
 app = Flask(__name__)
 
-@app.route("/")
-def loadPopularity():
+@app.route("/result/<name>")
+def result(name):
     triplets_file = 'https://static.turi.com/datasets/millionsong/10000.txt'
     song_df_1 = pd.read_table(triplets_file,header=None)
     song_df_1.columns = ['user_id', 'song_id', 'listen_count']
@@ -29,10 +30,30 @@ def loadPopularity():
     train_data, test_data = train_test_split(song_df, test_size=0.20, random_state=0)
     pr = Recommenders.popularity_recommender_py()
     pr.create(train_data, 'user_id', 'song')
-    user_id = users[5]
+    user_id = users[int(name)]
     song_table = pr.recommend(user_id)
-    song_by_popularity = song_table[0:10]		
-    return render_template('view.html',tables=[song_by_popularity.to_html(classes='song_by_popularity')],titles = ['na','Songs By popularity'])                       
+    song_by_popularity = song_table[0:10]  
+
+    #item-based collaborative filtering
+    iscf = Recommenders.item_similarity_recommender_py()
+    iscf.create(train_data, 'user_id', 'song')
+    user_id = users[int(name)]
+    user_items = iscf.get_user_items(user_id)
+
+    song_table1 = iscf.recommend(user_id)
+    song_by_collab = song_table1[0:10]
+
+    return render_template('view.html',tables=[song_by_popularity.to_html(classes='song_by_popularity'), song_by_collab.to_html(classes='song_by_collab')],titles = ['na','Songs By popularity','Songs by collaborative filtering'])                       
+
+#from UI on post it performs below function to get the user id
+@app.route('/seeResult',methods = ['POST', 'GET'])
+def seeResult():
+   if request.method == 'POST':
+      user = request.form['num']
+      return redirect(url_for('result',name = user))
+   else:
+      user = request.args.get('num')
+      return redirect(url_for('result',name = user))
 
 if __name__ == "__main__":
     app.run()
